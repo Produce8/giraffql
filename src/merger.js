8ced40f936be957bd20d750f44c276fa23c277a4
Brandon Bailey <brandon.bailey@produce8.com>
2022-02-18 19:23:51 -0400

    support custom directives

--- src/merger.js
+++ src/merger.js
@@ -2,12 +2,13 @@ import * as fs from 'fs';
 import path from 'path';
 import chalk from 'chalk';
 import figures from './figures';
-import { buildSchema, printSchema } from 'graphql';
+import { buildSchema } from 'graphql';
+import { printSchemaWithDirectives } from '@graphql-tools/utils'
 
-export function mergeSchemas(inputSchema, output) {
+export function giraffqlMergeSchemas(inputSchema, output) {
     try {
-        const schema = buildSchema(inputSchema.definition, {});
-        const byteData = printSchema(schema);
+        const schema = buildSchema(inputSchema.definition);
+        const byteData = printSchemaWithDirectives(schema);
         if(!fs.existsSync(path.dirname(output))) {
             fs.mkdirSync(path.dirname(output), {recursive: true}, (error) => {
                 console.error(error);
@@ -16,11 +17,10 @@ export function mergeSchemas(inputSchema, output) {
         fs.writeFileSync(output, byteData, (error) => {
             console.error(error);
         });
-
         console.log(chalk.green(`${figures.tick} Schema files merged to ${output}\n`));
-        return 0;
     } catch (error) {
         console.error(error)
         return 1;
     }
+    return 0;
 }
